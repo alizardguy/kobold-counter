4b1f27a4ce1bb1857173b6bc63b744dfa9184630
alizardguy <coyotehelp123@gmail.com>
2023-10-01 18:13:47 -0700

    fixed kobold counter

--- scripts/neosCount.js
+++ scripts/neosCount.js
@@ -1,26 +1,40 @@
 /* setup dates */
-var date1 = new Date('2020-10-19T02:58:30.559468Z');
-var date2 = new Date(); //current date
+var neosRegDate = new Date('2020-10-19T02:58:30.559468Z');
+var resoRegDate = new Date('2023-09-23T09:22:24');
+var currentDate = new Date(); //current date
 
 /* get difference */
-var differenceInTime = date2.getTime() - date1.getTime();
-var differenceInDays = differenceInTime / (1000 * 3600 * 24);
+var neosDifferenceInTime = currentDate.getTime() - neosRegDate.getTime();
+var neosDifferenceInDays = neosDifferenceInTime / (1000 * 3600 * 24);
+
+var resoDifferenceInTime = currentDate.getTime() - resoRegDate.getTime();
+var resoDifferenceInDays = resoDifferenceInTime / (1000 * 3600 * 24);
 
 /* convert to kobold sets */
-var koboldSets = differenceInDays / 11;
+var resoKoboldSets = resoDifferenceInDays / 11;
+var neosKoboldSets = neosDifferenceInDays / 11;
 
 /* floor values */
-differenceInDays = Math.floor(differenceInDays);
-koboldSets = Math.floor(koboldSets) + 1;
+neosDifferenceInDays = Math.floor(neosDifferenceInDays);
+neosKoboldSets = Math.floor(neosKoboldSets) + 1;
+
+resoDifferenceInDays = Math.floor(resoDifferenceInDays);
+resoKoboldSets = Math.floor(resoKoboldSets) + 1;
 
 /* debug output values */
-console.log(differenceInDays + " days");
-console.log(koboldSets + " legacy kobolds");
+console.log(resoDifferenceInDays + " days");
+console.log(resoKoboldSets + " legacy kobolds");
 
 /* get elements */
 var neosKoboldCount = document.getElementById("neosKoboldCount");
 var neoDayTotal = document.getElementById("neosDayTotal");
 
+var resoKoboldCount = document.getElementById("koboldCount");
+var resoDayTotal = document.getElementById("dayTotal");
+
 /* write elements */
-neosKoboldCount.textContent = koboldSets + " legacy kobolds";
-neoDayTotal.textContent = differenceInDays + " days since I've started Neos";
+neosKoboldCount.textContent = neosKoboldSets + " legacy kobolds";
+neoDayTotal.textContent = neosDifferenceInDays + " days since I've started Neos";
+
+resoKoboldCount.textContent = resoKoboldSets + " kobolds";
+resoDayTotal.textContent = resoDifferenceInDays + " days since I've started Resonite";
